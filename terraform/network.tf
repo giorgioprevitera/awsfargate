2b3290dd668dc2f3a14382734ef9af721c730084
Giorgio Previtera <giorgioprevitera@gmail.com>
2019-10-20 22:46:49 +0100

    Add base network
    - Create main VPC
    - Create subnets

---
+++ terraform/network.tf
@@ -0,0 +1,27 @@
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+locals {
+  az_count = length(data.aws_availability_zones.available.names)
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "private" {
+  count             = local.az_count
+  cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+  vpc_id            = aws_vpc.main.id
+}
+
+
+resource "aws_subnet" "public" {
+  count                   = local.az_count
+  cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index + local.az_count)
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  vpc_id                  = aws_vpc.main.id
+  map_public_ip_on_launch = true
+}
