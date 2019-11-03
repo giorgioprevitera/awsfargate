5dfd030183e5611a41656542d2c927f03822121b
Giorgio Previtera <giorgioprevitera@gmail.com>
2019-11-03 16:15:50 +0000

    Complete network resources

--- terraform/network.tf
+++ terraform/network.tf
@@ -9,7 +9,7 @@ locals {
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
   tags = {
-    Name = "${var.project_name}"
+    Name = var.project_name
   }
 }
 
@@ -33,4 +33,43 @@ resource "aws_subnet" "public" {
   tags = {
     Name = "${var.project_name}-public"
   }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route" "internet_access" {
+  route_table_id         = "aws_vpc.main.main_route_table_id"
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.gw.id
+}
+
+resource "aws_eip" "gw" {
+  count      = local.az_count
+  vpc        = true
+  depends_on = [aws_internet_gateway.gw]
+}
+
+resource "aws_nat_gateway" "gw" {
+  count         = local.az_count
+  allocation_id = aws_eip.gw[count.index].id
+  subnet_id     = aws_subnet.public[count.index].id
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+  count  = local.az_count
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.gw[count.index].id
+  }
+}
+
+resource "aws_route_table_association" "private" {
+  route_table_id = aws_route_table.private[count.index].id
+  subnet_id      = aws_subnet.private[count.index].id
+
+  count = local.az_count
 }
